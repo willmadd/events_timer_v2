2b11354bf59c3caf5a1d2dd24a2b6cf20ca55a91
William Maddicott <williammaddicott@Williams-iMac.local>
2021-02-22 11:17:14 +0000

    page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Events Countdown | The intro video creator for professional web and in person events</title>
 
         <!-- Fonts -->
         <!-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> -->
